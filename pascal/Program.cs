e9c6ce399d1ff97d9f7142b2a8036d4dc696e6ad
Ekaterina <missathlon08@gmail.com>
2022-09-27 14:55:42 +0300

    add Pochti rovno

--- pascal/Program.cs
+++ pascal/Program.cs
@@ -22,44 +22,23 @@ void InputMatrix(int[,] matrix)
 
 void PrintMatrix(int[,] matrix)
 {
+    
     Console.WriteLine();
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
+        for(int k=0;k<=matrix.Length-i;k++)
+                {
+                    Console.Write("  ");
+                }
                 for (int j = 0; j < matrix.GetLength(1); j++)
-            if(matrix[i,j]!=0) Console.Write(matrix[i, j] + " \t");
-            else Console.Write(" \t");
+            if(matrix[i,j]!=0) Console.Write(matrix[i, j] + "  ");
+            else Console.Write("   ");
         Console.WriteLine();
         Console.WriteLine();
     }
 }
 
-// void NewMatrix(int[,] matrix)
-// {
-//     int k;
-//     if (matrix.GetLength(0) % 2 == 0) k = matrix.GetLength(0) / 2-1;
-//     else k = (matrix.GetLength(0) + 1)/ 2-1;   
-
-// while(matrix[0,k]!=1)
-// {
-    
-// for (int i = matrix.GetLength(1)-3; i >=0; i--)
-// {
-//     int count =3;
-//     if(matrix[i,matrix.GetLength(0)-2]==0)
-//     {
-//     for (int j = matrix.GetLength(0)-2; j>0; j--)
-//     {   
-       
-//         int temp =matrix[i, j];        
-//         matrix[i, j] = matrix[i, j-1];
-//         matrix[i, j-1]=temp;
-//         count++; 
-//     }
-//     }   
-// }
-// }
-// }
-int[,] matrix = new int[N, N];
+int[,] matrix = new int[N,N];
 InputMatrix(matrix);
 PrintMatrix(matrix);
-// NewMatrix(matrix);
+
